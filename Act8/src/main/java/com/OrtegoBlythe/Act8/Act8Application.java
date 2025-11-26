61dc1db94df0711e4b0e7992595bc4695e4d3db2
blythedelleortego15-boop <blythedelleortego15@gmail.com>
2025-11-26 14:17:24 +0800

    Add files via upload

---
+++ Act8/src/main/java/com/OrtegoBlythe/Act8/Act8Application.java
@@ -0,0 +1,13 @@
+package com.OrtegoBlythe.Act8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Act8Application.class, args);
+	}
+
+}
---
+++ Act8/src/test/java/com/OrtegoBlythe/Act8/Act8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrtegoBlythe.Act8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Act8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
